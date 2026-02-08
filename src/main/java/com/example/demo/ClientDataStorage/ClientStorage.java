9b27238c7713276fd2f34a563ceddf0f8a36f8a0
Void2214 <osnacidaniel@gmail.com>
2026-02-08 10:59:48 +0200

    lectia 19

---
+++ src/main/java/com/example/demo/ClientDataStorage/ClientStorage.java
@@ -0,0 +1,25 @@
+package com.example.demo.ClientDataStorage;
+
+import com.example.demo.models.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ClientStorage {
+    private Map<Long, Client> clients = new ConcurrentHashMap<>();
+    private AtomicLong _id = new AtomicLong(1);
+
+    public List<Client> getAllClients(){
+        return new ArrayList<>(clients.values());
+    }
+
+    public Client save(Client client){
+        long id = _id.getAndIncrement();
+        client.setId(id);
+        clients.put(id, client);
+        return client;
+    }
+}
---
+++ src/main/java/com/example/demo/services/ClientService.java
@@ -0,0 +1,20 @@
+package com.example.demo.services;
+
+import com.example.demo.ClientDataStorage.ClientStorage;
+import com.example.demo.models.Client;
+
+import java.util.List;
+
+public class ClientService {
+    private final ClientStorage cs = new ClientStorage();
+    public Client CreateClient(Client client){
+        Client newClient = new Client(client.getUsername(),
+                                        client.getEmail(),
+                                        client.getPassword(),
+                                        client.getRole());
+        return cs.save(newClient);
+    }
+    public List<Client> findAll(){
+        return cs.getAllClients();
+    }
+}
